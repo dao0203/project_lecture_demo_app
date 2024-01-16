19ad0afe3bf4701efb55f25d0d701b3c80a8a034
dao0203 <youzaizuoteng872@gmail.com>
2024-01-17 02:32:27 +0900

    Add coupon detail screen to router

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:project_lecture_demo_app/feature/coupon/coupon_detail_screen.dart';
 import 'package:project_lecture_demo_app/feature/coupon/coupon_screen.dart';
 import 'package:project_lecture_demo_app/feature/demo/demo_login_screen.dart';
 import 'package:project_lecture_demo_app/splash_gate.dart';
@@ -25,5 +26,13 @@ final goRouter = GoRouter(
         child: CouponScreen(),
       ),
     ),
+    GoRoute(
+        path: '/coupon/detail',
+        pageBuilder: (context, state) {
+          final id = state.extra as String;
+          return MaterialPage(
+            child: CouponDetailScreen(id: id),
+          );
+        }),
   ],
 );
